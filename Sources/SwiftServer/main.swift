82d06bd4a54763ad684056301ca10a8ff8773e76
Anil Kumar T V <atv@apple.com>
2016-09-15 09:33:41 +0530

    Changes for port number

--- Sources/SwiftServer/main.swift
+++ Sources/SwiftServer/main.swift
@@ -13,9 +13,18 @@ let router = Router()
 
 router.all("/*", middleware: BodyParser())
 
+let portNumber: UInt16!
+
+if let arg = Process.arguments.last, value = UInt16(arg) {
+    portNumber = value
+} else {
+    print("Usage: \(Process.arguments.first!) portNumber")
+    exit(1)
+}
+
 // start the server
 print("starting server port: 8091")
-Kitura.addHTTPServer(onPort: 8091, with: router)
+Kitura.addHTTPServer(onPort: portNumber, with: router)
 
 print("Server listening on Port: 8091")
 Kitura.run()
