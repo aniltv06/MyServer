fc41bd5ca668f50c6a6342b146c46138c6d96dc8
Anil Kumar T V <anilkumar.tv@exilant.com>
2016-10-04 17:33:33 +0530

    Added html page for home

--- Sources/SwiftServer/main.swift
+++ Sources/SwiftServer/main.swift
@@ -15,6 +15,79 @@ import Kitura
     import Darwin.C
 #endif
 
+ //let folderPath = FileManager.default.currentDirectoryPath
+//let fileName = NSString(string: #file)
+//print(folderPath)
+//print(fileName)
+var folderPath = NSString(string: #file)
+while(folderPath.lastPathComponent != "MyServer"){
+    folderPath = folderPath.deletingLastPathComponent as NSString
+}
+
+var destinationPath = ("\(folderPath)/Packages/Kitura-1.0.0/Sources/Kitura/resources") as NSString
+print(folderPath)
+
+var sourcePath = ("\(folderPath)/Sources/SwiftServer/resources") as NSString
+
+let fileManager = FileManager.default
+
+// Delete 'subfolder' folder
+
+do {
+    try fileManager.removeItem(atPath: destinationPath as String)
+}
+catch let error as NSError {
+    print("Ooops! Something went wrong: \(error)")
+}
+do {
+ try fileManager.copyItem(atPath: sourcePath as String, toPath: destinationPath as String)
+} catch let error as NSError {
+        print("Ooops! Something went wrong: \(error)")
+    }
+
+private func getFilePath(for resource: String) -> String? {
+    let fileManager = FileManager.default
+    let potentialResource = getResourcePathBasedOnSourceLocation(for: resource)
+    
+    let fileExists = fileManager.fileExists(atPath: potentialResource)
+    if fileExists {
+        return potentialResource
+    } else {
+        return getResourcePathBasedOnCurrentDirectory(for: resource, withFileManager: fileManager)
+    }
+}
+
+private func getResourcePathBasedOnSourceLocation(for resource: String) -> String {
+    let fileName = NSString(string: #file)
+    let resourceFilePrefixRange: NSRange
+    let lastSlash = fileName.range(of: "/", options: .backwards)
+    if  lastSlash.location != NSNotFound {
+        resourceFilePrefixRange = NSMakeRange(0, lastSlash.location+1)
+    } else {
+        resourceFilePrefixRange = NSMakeRange(0, fileName.length)
+    }
+    return fileName.substring(with: resourceFilePrefixRange) + "resources/" + resource
+}
+
+private func getResourcePathBasedOnCurrentDirectory(for resource: String, withFileManager fileManager: FileManager) -> String? {
+    do {
+        let packagePath = fileManager.currentDirectoryPath + "/Packages"
+        let packages = try fileManager.contentsOfDirectory(atPath: packagePath)
+        for package in packages {
+            let potentalResource = "\(packagePath)/\(package)/Sources/Kitura/resources/\(resource)"
+            let resourceExists = fileManager.fileExists(atPath: potentalResource)
+            if resourceExists {
+                return potentalResource
+            }
+        }
+    } catch {
+        return nil
+    }
+    return nil
+}
+
+//var filePath = getFilePath(for: "index.html")
+
 let router = Router()
 
 router.all("/*", middleware: BodyParser())
---
+++ Sources/SwiftServer/resources/origin.js
@@ -0,0 +1,20 @@
+   var dependencies =
+{"links":[
+{"source":"Anil","dest":"CEO"},
+{"source":"CEO","dest":"VP1"},
+{"source":"CEO","dest":"VP2"},
+{"source":"VP1","dest":"Manager1"},
+{"source":"VP1","dest":"Manager2"},
+{"source":"VP2","dest":"Manager3"},
+{"source":"VP2","dest":"Manager4"},
+{"source":"Manager1","dest":"TL1"},
+{"source":"TL1","dest":"EMP1"},
+{"source":"TL1","dest":"EMP2"},
+{"source":"Manager2","dest":"EMP3"},
+{"source":"Manager2","dest":"EMP4"},
+{"source":"Manager3","dest":"EMP5"},
+{"source":"Manager3","dest":"EMP6"},
+{"source":"Manager4","dest":"EMP7"},
+{"source":"Manager4","dest":"EMP8"}
+],
+"source_files_count":263,"links_count":7}
